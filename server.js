71ea258854f9b071cc7740c4ce8b037efde4ed26
Nsikan <nsikane@live.co.uk>
2018-03-29 21:59:10 +0100

    Created repo

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+/* setting up mount paths for assets */
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/images_src', express.static(path.join(__dirname, 'images_src')));
+
+/* setting up server paths */
+app.get('/', (req, res) => res.sendFile('index.html', {
+    root: __dirname,
+}));
+app.get('*', (req, res) => res.sendFile('index.html', {
+    root: __dirname,
+}));
+
+app.listen(3000, () => {
+    console.log('now listening on port 3000');
+});
